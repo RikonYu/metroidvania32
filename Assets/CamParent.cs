6e166dc2aa7869a426c7c2697a90c3702fe90e43
Vermilion <grtrbwsword@gmail.com>
2026-05-16 13:30:48 +0800

    Implement player movement and camera follow

--- Assets/CamParent.cs
+++ Assets/CamParent.cs
@@ -1,18 +1,130 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CamParent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform target;
+    [SerializeField] private Camera targetCamera;
+    [SerializeField] private Vector2 deadZoneSize = new Vector2(4f, 2f);
+    [SerializeField] private Vector2 followOffset = new Vector2(0f, 1f);
+    [SerializeField] private float smoothTime = 0.15f;
+    [SerializeField] private bool drawGizmos = true;
+
+    private Room currentRoom;
+    private Vector3 smoothVelocity;
+
+    private Camera TargetCamera
     {
-        
+        get
+        {
+            if (targetCamera == null)
+            {
+                targetCamera = GetComponentInChildren<Camera>();
+            }
+
+            if (targetCamera == null)
+            {
+                targetCamera = Camera.main;
+            }
+
+            return targetCamera;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        if (target == null || currentRoom == null)
+        {
+            return;
+        }
+
+        Vector3 desired = GetDeadZoneAdjustedPosition();
+        desired = ClampToCurrentRoom(desired);
+        transform.position = Vector3.SmoothDamp(transform.position, desired, ref smoothVelocity, smoothTime);
+    }
+
+    public void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+
+    public void SetCurrentRoom(Room room)
+    {
+        currentRoom = room;
+    }
+
+    public void HardCutToTarget()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        Vector3 desired = target.position + (Vector3)followOffset;
+        transform.position = ClampToCurrentRoom(desired);
+        smoothVelocity = Vector3.zero;
+    }
+
+    public void HardCutToPosition(Vector3 position)
+    {
+        transform.position = ClampToCurrentRoom(position);
+        smoothVelocity = Vector3.zero;
+    }
+
+    private Vector3 GetDeadZoneAdjustedPosition()
+    {
+        Vector3 current = transform.position;
+        Vector3 desiredTarget = target.position + (Vector3)followOffset;
+        Vector2 halfDeadZone = deadZoneSize * 0.5f;
+
+        Vector3 desired = current;
+        desired.z = current.z;
+
+        float deltaX = desiredTarget.x - current.x;
+        if (Mathf.Abs(deltaX) > halfDeadZone.x)
+        {
+            desired.x = desiredTarget.x - Mathf.Sign(deltaX) * halfDeadZone.x;
+        }
+
+        float deltaY = desiredTarget.y - current.y;
+        if (Mathf.Abs(deltaY) > halfDeadZone.y)
+        {
+            desired.y = desiredTarget.y - Mathf.Sign(deltaY) * halfDeadZone.y;
+        }
+
+        return desired;
+    }
+
+    private Vector3 ClampToCurrentRoom(Vector3 position)
+    {
+        if (currentRoom == null || TargetCamera == null)
+        {
+            return position;
+        }
+
+        Rect rect = currentRoom.WorldRect;
+        float halfHeight = TargetCamera.orthographicSize;
+        float halfWidth = halfHeight * TargetCamera.aspect;
+
+        position.x = Mathf.Clamp(position.x, rect.xMin + halfWidth, rect.xMax - halfWidth);
+        position.y = Mathf.Clamp(position.y, rect.yMin + halfHeight, rect.yMax - halfHeight);
+        return position;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (!drawGizmos)
+        {
+            return;
+        }
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(transform.position, new Vector3(deadZoneSize.x, deadZoneSize.y, 0f));
+
+        if (currentRoom != null && TargetCamera != null)
+        {
+            Rect rect = currentRoom.WorldRect;
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireCube(rect.center, new Vector3(rect.width, rect.height, 0f));
+        }
     }
 }
--- Assets/MCController.cs
+++ Assets/MCController.cs
@@ -1,18 +1,218 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class MCController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private float jumpVelocity = 14f;
+    [SerializeField] private float maxFallSpeed = 24f;
+
+    [Header("Jump Feel")]
+    [SerializeField] private float coyoteTime = 0.08f;
+    [SerializeField] private float jumpBufferTime = 0.1f;
+    [SerializeField] private float fallGravityMultiplier = 1.8f;
+    [SerializeField] private float lowJumpGravityMultiplier = 2.2f;
+
+    [Header("Ground Check")]
+    [SerializeField] private Vector2 groundCheckOffset = new Vector2(0f, -1.05f);
+    [SerializeField] private Vector2 groundCheckSize = new Vector2(0.85f, 0.12f);
+    [SerializeField] private LayerMask movementGroundMask;
+    [SerializeField] private LayerMask safeGroundMask;
+
+    private Rigidbody2D body;
+    private float inputX;
+    private float coyoteCounter;
+    private float jumpBufferCounter;
+    private bool jumpHeld;
+    private bool inputLocked;
+    private Collider2D currentGround;
+
+    public bool IsGrounded { get; private set; }
+    public bool IsOnSafeGround { get; private set; }
+    public int FacingDirection { get; private set; } = 1;
+
+    public Vector2 Velocity
     {
-        
+        get { return body != null ? body.velocity : Vector2.zero; }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        body = GetComponent<Rigidbody2D>();
+        body.freezeRotation = true;
+        EnsureLayerMasks();
+    }
+
+    private void Reset()
+    {
+        EnsureLayerMasks();
+    }
+
+    private void Update()
+    {
+        if (inputLocked)
+        {
+            inputX = 0f;
+            jumpHeld = false;
+            return;
+        }
+
+        inputX = Input.GetAxisRaw("Horizontal");
+        jumpHeld = Input.GetKey(KeyCode.Space);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+
+        if (inputX > 0.01f)
+        {
+            FacingDirection = 1;
+        }
+        else if (inputX < -0.01f)
+        {
+            FacingDirection = -1;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateGroundedState();
+        UpdateTimers();
+        ApplyMovement();
+        ApplyJump();
+        ApplyExtraGravity();
+    }
+
+    public void SetInputLocked(bool locked)
+    {
+        inputLocked = locked;
+        if (locked)
+        {
+            inputX = 0f;
+            jumpBufferCounter = 0f;
+        }
+    }
+
+    public void TeleportTo(Vector3 position, int facingDirection)
+    {
+        transform.position = position;
+        SetFacingDirection(facingDirection);
+        ClearVelocity();
+        UpdateGroundedState();
+    }
+
+    public void ClearVelocity()
+    {
+        if (body != null)
+        {
+            body.velocity = Vector2.zero;
+        }
+    }
+
+    public Collider2D GetCurrentGround()
+    {
+        return currentGround;
+    }
+
+    private void ApplyMovement()
+    {
+        Vector2 velocity = body.velocity;
+        velocity.x = inputX * moveSpeed;
+        body.velocity = velocity;
+    }
+
+    private void ApplyJump()
+    {
+        if (jumpBufferCounter <= 0f || coyoteCounter <= 0f)
+        {
+            return;
+        }
+
+        Vector2 velocity = body.velocity;
+        velocity.y = jumpVelocity;
+        body.velocity = velocity;
+        jumpBufferCounter = 0f;
+        coyoteCounter = 0f;
+        IsGrounded = false;
+        IsOnSafeGround = false;
+    }
+
+    private void ApplyExtraGravity()
+    {
+        Vector2 velocity = body.velocity;
+        if (velocity.y < 0f)
+        {
+            velocity += Physics2D.gravity * ((fallGravityMultiplier - 1f) * Time.fixedDeltaTime);
+        }
+        else if (velocity.y > 0f && !jumpHeld)
+        {
+            velocity += Physics2D.gravity * ((lowJumpGravityMultiplier - 1f) * Time.fixedDeltaTime);
+        }
+
+        if (velocity.y < -maxFallSpeed)
+        {
+            velocity.y = -maxFallSpeed;
+        }
+
+        body.velocity = velocity;
+    }
+
+    private void UpdateTimers()
+    {
+        if (IsGrounded)
+        {
+            coyoteCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteCounter -= Time.fixedDeltaTime;
+        }
+
+        jumpBufferCounter -= Time.fixedDeltaTime;
+    }
+
+    private void UpdateGroundedState()
+    {
+        Vector2 center = (Vector2)transform.position + groundCheckOffset;
+        currentGround = Physics2D.OverlapBox(center, groundCheckSize, 0f, movementGroundMask);
+        IsGrounded = currentGround != null;
+        IsOnSafeGround = IsGrounded && IsColliderOnMask(currentGround, safeGroundMask);
+    }
+
+    private void SetFacingDirection(int facingDirection)
+    {
+        if (facingDirection == 0)
+        {
+            return;
+        }
+
+        FacingDirection = facingDirection > 0 ? 1 : -1;
+    }
+
+    private static bool IsColliderOnMask(Collider2D collider2D, LayerMask mask)
+    {
+        return collider2D != null && (mask.value & (1 << collider2D.gameObject.layer)) != 0;
+    }
+
+    private void EnsureLayerMasks()
+    {
+        if (movementGroundMask == 0)
+        {
+            movementGroundMask = LayerMask.GetMask(GameLayers.Ground, GameLayers.Platform);
+        }
+
+        if (safeGroundMask == 0)
+        {
+            safeGroundMask = LayerMask.GetMask(GameLayers.Ground);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = IsOnSafeGround ? Color.green : Color.yellow;
+        Vector2 center = (Vector2)transform.position + groundCheckOffset;
+        Gizmos.DrawWireCube(center, groundCheckSize);
     }
 }
